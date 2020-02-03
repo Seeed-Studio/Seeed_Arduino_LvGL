b83818fab673f03f138c296a1ecde6a6d0798295
LynnL4 <lht856@foxmail.com>
2020-02-03 20:48:11 +0800

    add test example

---
+++ examples/lv_tests/lv_test_group/lv_test_group.h
@@ -0,0 +1,52 @@
+/**
+ * @file lv_test_group.h
+ *
+ */
+
+#ifndef LV_TEST_GROUP_H
+#define LV_TEST_GROUP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lvgl.h"
+#ifdef LV_CONF_INCLUDE_SIMPLE
+#include "lv_ex_conf.h"
+#else
+#include "../lv_ex_conf.h"
+#endif
+
+#if LV_USE_GROUP && LV_USE_TESTS
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**
+ * Create base groups to test their functionalities
+ */
+lv_group_t *lv_test_group_1(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /* LV_USE_GROUP && LV_USE_TESTS */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_TEST_BAR_H*/
---
+++ examples/lv_tests/lv_test_theme/lv_test_theme_2.h
@@ -0,0 +1,52 @@
+/**
+ * @file lv_test_theme_2.h
+ *
+ */
+
+#ifndef LV_TEST_THEME_2_H
+#define LV_TEST_THEME_2_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lvgl.h"
+#ifdef LV_CONF_INCLUDE_SIMPLE
+#include "lv_ex_conf.h"
+#else
+#include "../lv_ex_conf.h"
+#endif
+
+#if LV_USE_TESTS
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**
+ * Test run time theme change
+ */
+void lv_test_theme_2(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /*LV_USE_TESTS*/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_TEST_THEME_2_H*/
